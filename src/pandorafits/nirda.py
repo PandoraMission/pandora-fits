68c5dd84f5e8de0a783e9832dd760cf64d8588eb
Christina Hedges <christina.l.hedges@nasa.gov>
2025-11-24 12:00:27 -0500

    fix tests

--- src/pandorafits/nirda.py
+++ src/pandorafits/nirda.py
@@ -11,25 +11,25 @@ __all__ = [
 
 
 class NIRDAFFILevel0HDUList(PandoraHDUList):
-    filename = FORMATSDIR + "NIRDA/level0-ffi_nirda.xlsx"
+    filename = FORMATSDIR + "nirda/level0-ffi_nirda.xlsx"
     reference = NIRDAReference
 
 
 class NIRDAFFILevel1HDUList(PandoraHDUList):
-    filename = FORMATSDIR + "NIRDA/level1-ffi_nirda.xlsx"
+    filename = FORMATSDIR + "nirda/level1-ffi_nirda.xlsx"
     reference = NIRDAReference
 
 
 class NIRDALevel0HDUList(PandoraHDUList):
-    filename = FORMATSDIR + "NIRDA/level0_nirda.xlsx"
+    filename = FORMATSDIR + "nirda/level0_nirda.xlsx"
     reference = NIRDAReference
 
 
 class NIRDALevel1HDUList(PandoraHDUList):
-    filename = FORMATSDIR + "NIRDA/level1_nirda.xlsx"
+    filename = FORMATSDIR + "nirda/level1_nirda.xlsx"
     reference = NIRDAReference
 
 
 class NIRDALevel2HDUList(PandoraHDUList):
-    filename = FORMATSDIR + "NIRDA/level2_nirda.xlsx"
+    filename = FORMATSDIR + "nirda/level2_nirda.xlsx"
     reference = NIRDAReference
